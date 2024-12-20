fd8420b1fe3d8c9ba1c75e4288815afee774de3e
Lorenzo Morelli <lorenzomorelli95@gmail.com>
2024-12-20 11:50:20 +0100

    implementing .env files to store apikey securely!

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -1,9 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (request, next) => {
-
-  //put here your api key from https://polygon.io/dashboard/api-keys (sign up if you don't have one)
-  const apiKey = 'your-api-key';
+  //put your free api key from https://polygon.io/dashboard/api-keys here (sign up if you don't have one)
+  //tocca capire come togliere l'info da qua e metterla in un enviroment e soprattutto come legare questa cosa con vercel.
+  const apiKey = import.meta.env['NG_APP_API_KEY'];
   request = request.clone({
     setParams: {
       apiKey: apiKey,
